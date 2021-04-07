56c9345fc0ff4a270fe0deb11301336654c39a3b
Home <Home@DQ.com>
2021-04-07 21:57:07 +0800

    完成中国技术网js破解

--- test.py
+++ test.py
@@ -7,26 +7,64 @@
 
 
 import requests
+import execjs
+# url = "http://mall.ckcest.cn/mall/listContent.ilf"
+# ret = requests.get(url)
+# arg1 = ret.text.split("arg1=")[-1].split(";")[0].strip('"').strip("'")
+# print(arg1)
+# with open(r'D:\Code\JSReverse\zggckjzszx\test.js', encoding='utf-8', mode='r') as f:
+#     JsData = f.read()
+# psd = execjs.compile(JsData).call('get_arg3_value', arg1)
+
+import requests
 
 cookies = {
-    'wwwcookie': '18205800',
-    '__jsluid_h': 'a7ab2989e04879aad8df0bf7c03b1b7f',
-    'UM_distinctid': '178a73df0116c2-0785edad9f769a-c3f3568-240000-178a73df012a35',
-    'CNZZDATA1276383350': '1401340496-1617710696-https^%^253A^%^252F^%^252Fwww.baidu.com^%^252F^%^7C1617710696',
+    '_Collect_ISNEW': '1617799373226',
+    '_Collect_UD': 'b077b785-7842-430c-843e-092f560e4d50',
+    '_uab_collina': '161780090977104704735464',
+    '_YS_userAccect': '6d8dd931-78c6-4926-83b2-19deb6aa5c19',
+    '_Collect_SN': '0',
+    'JSESSIONID': '7088752567F0116296839F95DC940900',
+    'Hm_lvt_789fd650fa0be6a2a064d019d890b87f': '1617799373',
+    'acw_tc': '276aedda16178034084306783e5eb8cf3e66f0c5f9ccccc484d73bb8f45976',
+    'Hm_lpvt_789fd650fa0be6a2a064d019d890b87f': '1617803456',
+    'ssxmod_itna': 'YqfOGKBKDK0KCDlSD+obkneDvP7Iq+6IfAxXdD/AY+DnqD=GFDK40EYgwtup+p=obrBTX1L7=YcGP5yEnu+Wd+G40aDbqGkzmir4GGjxBYDQxAYDGDDPkDj4ibDY4tBnwxWKDwDlKDgD7QlUqDfDDLnbGAnYeLnmqdV0qiDAuqgAG+nD0tDIqGXYBuPbqDBDanPDYpWKwN/DBQD7kiBn4iDC2mqFSGYA4wfFbqsDYv=S7wY8YhKehgq3UfQf7qI494GXDDAKi4GPD===',
+    'ssxmod_itna2': 'YqfOGKBKDK0KCDlSD+obkneDvP7Iq+6IfAxNG9i5xBuwSD7P32BFfBp7NFCZhzRnrYrXC2DSdFqwSBElg2Atk8L8bEHLUhptC5ftPEFpDRE37p5ix7jPjDjKD2FYD===',
+    'acw_sc__v2': '606db8c2db355082f0e9d00cdcd615b24bc7cfc9',
 }
 
 headers = {
     'Connection': 'keep-alive',
-    'Cache-Control': 'max-age=0',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
     'DNT': '1',
-    'Upgrade-Insecure-Requests': '1',
+    'X-Requested-With': 'XMLHttpRequest',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Referer': 'http://mall.ckcest.cn/mall/list.html',
     'Accept-Language': 'zh-CN,zh;q=0.9',
-    'If-None-Match': 'W/^\\^24bb7-5bf4c1936e140^\\^',
-    'If-Modified-Since': 'Tue, 06 Apr 2021 11:27:25 GMT',
 }
 
-response = requests.get('http://www.samr.gov.cn/', headers=headers, cookies=cookies, verify=False)
+params = (
+    ('dbId', '1000'),
+    ('text', ''),
+    ('express', ''),
+    ('secondSearchExpress', ''),
+    ('fieldSearchExpress', ''),
+    ('fieldSearchEntity', ''),
+    ('order', '1'),
+    ('startYear', '1700'),
+    ('endYear', '2099'),
+    ('page', '1'),
+    ('limit', '10'),
+    ('model', '1'),
+    ('isadvanced', '0'),
+    ('picture', ''),
+    ('uuid', ''),
+)
+
+response = requests.get('http://mall.ckcest.cn/mall/listContent.ilf', headers=headers, params=params, cookies=cookies, verify=False)
+
+#NB. Original query string below. It seems impossible to parse and
+#reproduce query strings 100% accurately so the one below is given
+#in case the reproduced version is not "correct".
+# response = requests.get('http://mall.ckcest.cn/mall/listContent.ilf?dbId=1000&text=&express=&secondSearchExpress=&fieldSearchExpress=&fieldSearchEntity=&order=1&startYear=1700&endYear=2099&page=1&limit=10&model=1&isadvanced=0&picture=&uuid=', headers=headers, cookies=cookies, verify=False)
 print(response.text)
-print(response)
---
+++ zggckjzszx/get_html.py
@@ -0,0 +1,96 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# @author :   DQ
+# @file   :   get_html.py
+# @time   :   2021/04/07
+
+import execjs
+import requests
+
+JsData = """function get_arg3_value(arg1) {
+ var posList = [15, 35, 29, 24, 33, 16, 1, 38, 10, 9, 19, 31, 40, 27, 22, 23, 25, 13, 6, 11, 39, 18, 20, 8, 14, 21, 32, 26, 2, 30, 7, 4, 17, 5, 3, 28, 34, 37, 12, 36];
+ var mask = "3000176000856006061501533003690027800375";
+ var outPutList = [];
+ var arg2 = "";
+ var arg3 = "";
+ for (var i = 0; i < 40; i++) {
+     var this_i = arg1[i];
+     for (var j = 0; j < 40; j++) {
+         if (posList[j] == i + 1) {
+             outPutList[j] = this_i
+         }
+     }
+ }
+ arg2 = outPutList["join"]("");
+ for (var i = 0; i < 40 && i < 40; i += 2) {
+     var GxjQsM = "1|4|3|0|2"["split"]("|")
+         , QoWazb = 0;
+     while (!![]) {
+         switch (GxjQsM[QoWazb++]) {
+             case "0":
+                 if (xorChar["length"] == 1) {
+                     xorChar = "0" + xorChar
+                 }
+                 continue;
+             case "1":
+                 var strChar = parseInt(arg2["slice"](i, i + 2), 16);
+                 continue;
+             case "2":
+                 arg3 += xorChar;
+                 continue;
+             case "3":
+                 var xorChar = (strChar ^ maskChar)["toString"](16);
+                 continue;
+             case "4":
+                 var maskChar = parseInt(mask["slice"](i, i + 2), 16);
+                 continue
+         }
+         break
+     }
+ }
+ var expiredate = new Date();
+ expiredate["setTime"](expiredate["getTime"]() + 3600 * 1000);
+ var theHost = "mall.ckcest.cn"
+     , theHostSplit = theHost.split(".")
+     , theHostSplitLength = theHostSplit.length;
+ !/^(\d+\.)*\d+$/.test(theHost) && theHostSplitLength > 2 && ("com.cn" != (theHost = theHostSplit[theHostSplitLength - 2] + "." + theHostSplit[theHostSplitLength - 1]) && "gov.cn" != theHost && "org.cn" != theHost && "net.cn" != theHost || (theHost = theHostSplit[theHostSplitLength - 3] + "." + theHost));
+ return arg3
+ }"""
+
+def calculate_m_value(arg1):
+    psd = execjs.compile(JsData).call('get_arg3_value', arg1)
+    return psd
+
+
+def get_arg1():
+    ret = requests.get(url)
+    arg1 = ret.text.split("arg1=")[-1].split(";")[0].strip('"').strip("'")
+    arg3 = calculate_m_value(arg1)
+    get_html(arg3)
+
+
+def get_html(arg3):
+    cookies = {
+        'acw_sc__v2': arg3,
+    }
+    headers = {
+        'Connection': 'keep-alive',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'DNT': '1',
+        'X-Requested-With': 'XMLHttpRequest',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
+        'Referer': 'http://mall.ckcest.cn/mall/list.html',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+    }
+
+    ret = requests.get(url, cookies=cookies, headers=headers)
+    ret.encoding = ret.apparent_encoding
+    print(ret.encoding)
+    print(ret.text)
+
+
+if __name__ == '__main__':
+    # calculate_m_value()
+    url = "http://mall.ckcest.cn/mall/listContent.ilf?dbId=1000&text=&express=&secondSearchExpress=&fieldSearchExpress=&fieldSearchEntity=&order=1&startYear=1700&endYear=2099&page=1&limit=10&model=1&isadvanced=0&picture=&uuid="
+    get_arg1()
